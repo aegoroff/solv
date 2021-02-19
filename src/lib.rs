70aa8cac0a3e3abc503beb323ae3aed02ed1a6bf
egoroff <egoroff@gmail.com>
2021-02-19 22:14:21 +0300

    continue experiments

--- src/lib.rs
+++ src/lib.rs
@@ -5,10 +5,15 @@ lalrpop_mod!(pub solt);
 #[derive(Debug)]
 pub enum OpCode {
     Comma,
+    Quote,
     Dot,
     Eq,
     ParenOpen,
     ParenClose,
+}
+
+#[derive(Debug)]
+pub enum Expr {
     Comment(String),
     DigitOrDot(String),
     Guid(String),
@@ -17,6 +22,9 @@ pub enum OpCode {
     BareStr(String),
     Version(String, String),
     FirstLine,
+    ProjectBegin,
+    ProjectEnd,
+    ProjectType(String),
 }
 
 #[cfg(test)]
@@ -31,6 +39,8 @@ Microsoft Visual Studio Solution File, Format Version 12.00
 # Visual Studio 2013
 VisualStudioVersion = 12.0.31101.0
 MinimumVisualStudioVersion = 10.0.40219.1
+Project("{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}") = "DataVirtualization", "DataVirtualization\DataVirtualization.csproj", "{8102706C-AA37-4250-8889-1240FEB6F92F}"
+EndProject
 "#).unwrap();
         println!("{:#?}", result)
         //assert_eq!(result, "AB");
