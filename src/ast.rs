bb27a0916b117eb34a1a3891bc1991a105851019
egoroff <egoroff@gmail.com>
2021-02-27 15:11:31 +0300

    from simplified

--- src/ast.rs
+++ src/ast.rs
@@ -221,16 +221,18 @@ impl<'input> From<&'input str> for ProjectConfigs<'input> {
         let mut it = trail.split(".ActiveCfg");
         let mut platform = it.next().unwrap_or("");
         if platform.len() == trail.len() {
-            let mut it = trail.chars().rev();
+            let it = trail.chars().rev();
             let mut dot_count = 0;
-            let mut cut_count = 0;
             const SKIP_DOTS: usize = 2;
 
-            while dot_count < SKIP_DOTS {
-                cut_count += it.by_ref().take_while(|ch| *ch != '.').count();
-                dot_count += 1;
-            }
-            platform = &trail[..trail.len() - (cut_count + SKIP_DOTS)];
+            let cut_count = it.take_while(|ch| {
+                if *ch == '.' {
+                    dot_count += 1;
+                }
+                dot_count < SKIP_DOTS
+            }).count() + 1; // Last dot
+
+            platform = &trail[..trail.len() - cut_count];
         }
 
         let mut configs = Vec::new();
