cdf6050afb6e6ef8dcfac9c2658b2ec4770df7a6
Alexander Egorov <egoroff@gmail.com>
2022-10-15 21:50:22 +0300

    feat: Shell completion script generation added

--- solv/src/main.rs
+++ solv/src/main.rs
@@ -1,5 +1,6 @@
-use clap::{command, Command, ArgAction};
-use std::time::Instant;
+use clap::{command, ArgAction, ArgMatches, Command};
+use clap_complete::{generate, Shell};
+use std::{io, time::Instant};
 
 #[macro_use]
 extern crate clap;
@@ -12,37 +13,53 @@ fn main() {
     let matches = app.get_matches();
 
     let debug = matches.get_flag("debug");
-    let empty = String::default();
 
-    if let Some(cmd) = matches.subcommand_matches("d") {
-        if let Some(path) = cmd.get_one::<String>(PATH) {
-            let now = Instant::now();
-            let only_problems = cmd.get_flag("problems");
-            let extension = cmd.get_one::<String>("ext").unwrap_or(&empty);
-
-            let is_info = cmd.get_flag(INFO_FLAG);
-            let mut consumer = solv::new_consumer(debug, !is_info, only_problems);
-            let scanned = solp::scan(path, extension, consumer.as_consume());
-
-            println!();
-
-            print!("{}", consumer);
-
-            println!("{:>20} {}", "solutions scanned:", scanned);
-
-            println!(
-                "{:>20} {}",
-                "elapsed:",
-                humantime::format_duration(now.elapsed())
-            );
-        }
+    match matches.subcommand() {
+        Some(("d", cmd)) => scan_directory(cmd, debug),
+        Some(("s", cmd)) => scan_file(cmd, debug),
+        Some(("completion", cmd)) => print_completions(cmd),
+        _ => {}
     }
-    if let Some(cmd) = matches.subcommand_matches("s") {
-        if let Some(path) = cmd.get_one::<String>(PATH) {
-            let is_info = cmd.get_flag(INFO_FLAG);
-            let mut consumer = solv::new_consumer(debug, !is_info, false);
-            solp::parse_file(path, consumer.as_consume());
-        }
+}
+
+fn scan_file(cmd: &ArgMatches, debug: bool) {
+    if let Some(path) = cmd.get_one::<String>(PATH) {
+        let is_info = cmd.get_flag(INFO_FLAG);
+        let mut consumer = solv::new_consumer(debug, !is_info, false);
+        solp::parse_file(path, consumer.as_consume());
+    }
+}
+
+fn scan_directory(cmd: &ArgMatches, debug: bool) {
+    let empty = String::default();
+    if let Some(path) = cmd.get_one::<String>(PATH) {
+        let now = Instant::now();
+        let only_problems = cmd.get_flag("problems");
+        let extension = cmd.get_one::<String>("ext").unwrap_or(&empty);
+
+        let is_info = cmd.get_flag(INFO_FLAG);
+        let mut consumer = solv::new_consumer(debug, !is_info, only_problems);
+        let scanned = solp::scan(path, extension, consumer.as_consume());
+
+        println!();
+
+        print!("{}", consumer);
+
+        println!("{:>20} {}", "solutions scanned:", scanned);
+
+        println!(
+            "{:>20} {}",
+            "elapsed:",
+            humantime::format_duration(now.elapsed())
+        );
+    }
+}
+
+fn print_completions(matches: &ArgMatches) {
+    let mut cmd = build_cli();
+    let bin_name = cmd.get_name().to_string();
+    if let Some(generator) = matches.get_one::<Shell>("generator") {
+        generate(*generator, &mut cmd, bin_name, &mut io::stdout());
     }
 }
 
@@ -103,5 +120,15 @@ fn build_cli() -> Command {
                         .help("Sets solution path to analyze")
                         .required(true),
                 ),
+        )
+        .subcommand(
+            Command::new("completion")
+            .about("Generate the autocompletion script for the specified shell")
+            .arg(
+                arg!([generator])
+                    .value_parser(value_parser!(Shell))
+                    .required(true)
+                    .index(1),
+            )
         )
 }
