a5f11b70a8f1465228e3348983acf9853eee4af6
Alexander Egorov <egoroff@gmail.com>
2023-02-07 00:06:22 +0300

    fix: fuzzer code corrected

--- solp/fuzz/fuzz_targets/parse.rs
+++ solp/fuzz/fuzz_targets/parse.rs
@@ -1,21 +1,6 @@
 #![no_main]
 use libfuzzer_sys::fuzz_target;
-use solp::ast::Solution;
-use solp::Consume;
 
 fuzz_target!(|data: &str| {
-    let mut c = Consumer {};
-    solp::parse(&mut c, data);
+    solp::parse(data);
 });
-
-struct Consumer {}
-
-impl Consume for Consumer {
-    fn ok(&mut self, _path: &str, _solution: &Solution) {}
-
-    fn err(&self, _path: &str) {}
-
-    fn is_debug(&self) -> bool {
-        return false;
-    }
-}
