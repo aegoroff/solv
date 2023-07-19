d33506203b5b5d47da18e706841acd87a3559cc2
egoroff <egoroff@gmail.com>
2023-07-19 11:09:31 +0300

    refactor: Validator trait added and all separate validation actions moved to different validators

--- solv/src/validate.rs
+++ solv/src/validate.rs
@@ -9,6 +9,11 @@ use std::fmt;
 use std::fmt::Display;
 use std::path::PathBuf;
 
+trait Validator {
+    fn validate(&mut self) -> bool;
+    fn results(&self);
+}
+
 pub struct Validate {
     show_only_problems: bool,
 }
@@ -22,76 +27,25 @@ impl Validate {
 
 impl Consume for Validate {
     fn ok(&mut self, path: &str, solution: &Solution) {
-        let not_found = search_not_found(path, solution);
+        let mut not_found_validator = NotFouund::new(path, solution);
+        let mut danglings_validator = Danglings::new(solution);
+        let mut missings_validator = Missings::new(solution);
+        let mut cycles_validator = Cycles::new(solution);
 
-        let danglings = search_dangling_configs(solution);
-
-        let missings = search_missing(solution);
-
-        let mut space = DfsSpace::new(&solution.dependencies);
-        let cycle_detected =
-            petgraph::algo::toposort(&solution.dependencies, Some(&mut space)).is_err();
-
+        let not_found_result = not_found_validator.validate();
+        let danglings_result = danglings_validator.validate();
+        let missings_result = missings_validator.validate();
+        let cycles_result = cycles_validator.validate();
         let no_problems =
-            danglings.is_empty() && not_found.is_empty() && missings.is_empty() && !cycle_detected;
+            danglings_result && not_found_result && missings_result && cycles_result;
 
         if !no_problems || !self.show_only_problems {
             ux::print_solution_path(path);
         }
-
-        if cycle_detected {
-            println!(
-                " {}",
-                "  Solution contains project dependencies cycles"
-                    .dark_red()
-                    .bold()
-            );
-            println!();
-        }
-
-        if !(danglings.is_empty()) {
-            println!(
-                " {}",
-                "  Solution contains dangling project configurations that can be safely removed:"
-                    .dark_yellow()
-                    .bold()
-            );
-            println!();
-            ux::print_one_column_table("Project ID", danglings.iter().map(std::string::String::as_str));
-        }
-
-        if !(not_found.is_empty()) {
-            println!(
-                " {}",
-                "  Solution contains unexist projects:".dark_yellow().bold()
-            );
-            println!();
-            let items: Vec<&str> = not_found
-                .iter()
-                .filter_map(|p| p.as_path().to_str())
-                .collect();
-            ux::print_one_column_table("Path", items.into_iter());
-        }
-
-        if !(missings.is_empty()) {
-            println!(" {}", "  Solution contains project configurations that are outside solution's configuration|platform list:".dark_yellow().bold());
-            println!();
-
-            let mut table = Table::new();
-
-            let fmt = ux::new_format();
-            table.set_format(fmt);
-            table.set_titles(row![bF=> "Project ID", "Configuration|Platform"]);
-
-            for (id, configs) in &missings {
-                for config in configs.iter() {
-                    table.add_row(row![*id, format!("{}|{}", config.config, config.platform)]);
-                }
-            }
-
-            table.printstd();
-            println!();
-        }
+        cycles_validator.results();
+        danglings_validator.results();
+        not_found_validator.results();
+        missings_validator.results();
 
         if !self.show_only_problems && no_problems {
             println!(
@@ -113,60 +67,212 @@ impl Display for Validate {
     }
 }
 
-fn search_not_found<'a>(path: &'a str, solution: &'a Solution) -> BTreeSet<PathBuf> {
-    let dir = crate::parent_of(path);
-    solution
-        .iterate_projects()
-        .filter_map(|p| {
-            let full_path = crate::make_path(dir, p.path);
-            if full_path.canonicalize().is_ok() {
-                None
-            } else {
-                Some(full_path)
-            }
-        })
-        .collect()
+struct NotFouund<'a> {
+    path: &'a str,
+    solution: &'a Solution<'a>,
+    bad_paths: BTreeSet<PathBuf>,
 }
 
-fn search_dangling_configs(solution: &Solution) -> BTreeSet<String> {
-    let project_ids: FnvHashSet<String> = solution
-        .iterate_projects()
-        .map(|p| p.id.to_uppercase())
-        .collect();
-
-    solution
-        .project_configs
-        .iter()
-        .map(|p| p.project_id.to_uppercase())
-        .collect::<FnvHashSet<String>>()
-        .difference(&project_ids)
-        .cloned()
-        .collect()
+impl<'a> NotFouund<'a> {
+    pub fn new(path: &'a str, solution: &'a Solution<'a>) -> Self {
+        Self {
+            path,
+            solution,
+            bad_paths: BTreeSet::new(),
+        }
+    }
 }
 
-fn search_missing<'a>(solution: &'a Solution<'a>) -> Vec<(&'a str, Vec<&'a Conf>)> {
-    let solution_platforms_configs = solution
-        .solution_configs
-        .iter()
-        .collect::<FnvHashSet<&Conf>>();
+impl<'a> Validator for NotFouund<'a> {
+    fn validate(&mut self) -> bool {
+        let dir = crate::parent_of(self.path);
+        self.bad_paths = self
+            .solution
+            .iterate_projects()
+            .filter_map(|p| {
+                let full_path = crate::make_path(dir, p.path);
+                if full_path.canonicalize().is_ok() {
+                    None
+                } else {
+                    Some(full_path)
+                }
+            })
+            .collect();
+        self.bad_paths.is_empty()
+    }
 
-    solution
-        .project_configs
-        .iter()
-        .filter_map(|pc| {
-            let diff = pc
-                .configs
-                .iter()
-                .collect::<FnvHashSet<&Conf>>()
-                .difference(&solution_platforms_configs)
-                .copied()
-                .collect::<Vec<&Conf>>();
+    fn results(&self) {
+        if self.bad_paths.is_empty() {
+            return;
+        }
+        println!(
+            " {}",
+            "  Solution contains unexist projects:".dark_yellow().bold()
+        );
+        println!();
+        let items: Vec<&str> = self
+            .bad_paths
+            .iter()
+            .filter_map(|p| p.as_path().to_str())
+            .collect();
+        ux::print_one_column_table("Path", items.into_iter());
+    }
+}
 
-            if diff.is_empty() {
-                None
-            } else {
-                Some((pc.project_id, diff))
+struct Danglings<'a> {
+    solution: &'a Solution<'a>,
+    danglings: BTreeSet<String>,
+}
+
+impl<'a> Danglings<'a> {
+    pub fn new(solution: &'a Solution<'a>) -> Self {
+        Self {
+            solution,
+            danglings: BTreeSet::new(),
+        }
+    }
+}
+
+impl<'a> Validator for Danglings<'a> {
+    fn validate(&mut self) -> bool {
+        let project_ids: FnvHashSet<String> = self
+            .solution
+            .iterate_projects()
+            .map(|p| p.id.to_uppercase())
+            .collect();
+
+        self.danglings = self
+            .solution
+            .project_configs
+            .iter()
+            .map(|p| p.project_id.to_uppercase())
+            .collect::<FnvHashSet<String>>()
+            .difference(&project_ids)
+            .cloned()
+            .collect();
+        self.danglings.is_empty()
+    }
+
+    fn results(&self) {
+        if self.danglings.is_empty() {
+            return;
+        }
+        println!(
+            " {}",
+            "  Solution contains dangling project configurations that can be safely removed:"
+                .dark_yellow()
+                .bold()
+        );
+        println!();
+        ux::print_one_column_table(
+            "Project ID",
+            self.danglings.iter().map(std::string::String::as_str),
+        );
+    }
+}
+
+struct Missings<'a> {
+    solution: &'a Solution<'a>,
+    missings: Vec<(&'a str, Vec<&'a Conf<'a>>)>,
+}
+
+impl<'a> Missings<'a> {
+    pub fn new(solution: &'a Solution<'a>) -> Self {
+        Self {
+            solution,
+            missings: vec![],
+        }
+    }
+}
+
+impl<'a> Validator for Missings<'a> {
+    fn validate(&mut self) -> bool {
+        let solution_platforms_configs = self
+            .solution
+            .solution_configs
+            .iter()
+            .collect::<FnvHashSet<&Conf>>();
+
+        self.missings = self
+            .solution
+            .project_configs
+            .iter()
+            .filter_map(|pc| {
+                let diff = pc
+                    .configs
+                    .iter()
+                    .collect::<FnvHashSet<&Conf>>()
+                    .difference(&solution_platforms_configs)
+                    .copied()
+                    .collect::<Vec<&Conf>>();
+
+                if diff.is_empty() {
+                    None
+                } else {
+                    Some((pc.project_id, diff))
+                }
+            })
+            .collect();
+        self.missings.is_empty()
+    }
+
+    fn results(&self) {
+        if self.missings.is_empty() {
+            return;
+        }
+
+        println!(" {}", "  Solution contains project configurations that are outside solution's configuration|platform list:".dark_yellow().bold());
+        println!();
+
+        let mut table = Table::new();
+
+        let fmt = ux::new_format();
+        table.set_format(fmt);
+        table.set_titles(row![bF=> "Project ID", "Configuration|Platform"]);
+
+        for (id, configs) in &self.missings {
+            for config in configs.iter() {
+                table.add_row(row![*id, format!("{}|{}", config.config, config.platform)]);
             }
-        })
-        .collect()
+        }
+
+        table.printstd();
+        println!();
+    }
+}
+
+struct Cycles<'a> {
+    solution: &'a Solution<'a>,
+    cycles_detected: bool,
+}
+
+impl<'a> Cycles<'a> {
+    pub fn new(solution: &'a Solution<'a>) -> Self {
+        Self {
+            solution,
+            cycles_detected: false,
+        }
+    }
+}
+
+impl<'a> Validator for Cycles<'a> {
+    fn validate(&mut self) -> bool {
+        let mut space = DfsSpace::new(&self.solution.dependencies);
+        self.cycles_detected =
+            petgraph::algo::toposort(&self.solution.dependencies, Some(&mut space)).is_err();
+        !self.cycles_detected
+    }
+
+    fn results(&self) {
+        if !self.cycles_detected {
+            return;
+        }
+        println!(
+            " {}",
+            "  Solution contains project dependencies cycles"
+                .dark_red()
+                .bold()
+        );
+        println!();
+    }
 }
