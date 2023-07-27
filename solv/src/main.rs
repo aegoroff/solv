cd6a8cc69424c6da697e691558e834612ec07b1e
Alexander Egorov <egoroff@gmail.com>
2023-07-27 21:32:29 +0300

    feat: Show error if path specified cannot  be used

--- solv/src/main.rs
+++ solv/src/main.rs
@@ -1,5 +1,6 @@
 use clap::{command, ArgAction, ArgMatches, Command};
 use clap_complete::{generate, Shell};
+use color_eyre::eyre::{Context, Result};
 use solp::Consume;
 use solv::info::Info;
 use solv::nuget::Nuget;
@@ -10,7 +11,6 @@ use std::{
     io,
     time::{Duration, Instant},
 };
-use color_eyre::eyre::Result;
 
 #[macro_use]
 extern crate clap;
@@ -29,63 +29,66 @@ fn main() -> Result<()> {
         Some(("info", cmd)) => info(cmd),
         Some(("nuget", cmd)) => nuget(cmd),
         Some(("completion", cmd)) => print_completions(cmd),
-        _ => {}
+        _ => Ok(()),
+    }
+}
+
+fn validate(cmd: &ArgMatches) -> Result<()> {
+    let only_problems = cmd.get_flag("problems");
+
+    let mut consumer = Validate::new(only_problems);
+    scan_path(cmd, &mut consumer)
+}
+
+fn info(cmd: &ArgMatches) -> Result<()> {
+    let mut consumer = Info::new();
+    scan_path(cmd, &mut consumer)
+}
+
+fn nuget(cmd: &ArgMatches) -> Result<()> {
+    let only_mismatched = cmd.get_flag("mismatch");
+    let fail_if_mismatched = cmd.get_flag("fail");
+    let mut consumer = Nuget::new(only_mismatched);
+    let result = scan_path(cmd, &mut consumer);
+    if consumer.mismatches_found && fail_if_mismatched {
+        std::process::exit(exitcode::SOFTWARE);
+    }
+    result
+}
+
+fn scan_path<C: Consume + Display>(cmd: &ArgMatches, consumer: &mut C) -> Result<()> {
+    if let Some(path) = cmd.get_one::<String>(PATH) {
+        let metadata =
+            fs::metadata(path).wrap_err_with(|| format!("Failed to use path: {path}"))?;
+        if metadata.is_dir() {
+            let now = Instant::now();
+            let empty = String::default();
+            let extension = cmd.get_one::<String>("ext").unwrap_or(&empty);
+            solp::parse_dir(path, extension, consumer);
+
+            print!("{consumer}");
+
+            let duration = now.elapsed().as_millis();
+            let duration = Duration::from_millis(duration as u64);
+            println!("{:>2} {}", "elapsed:", humantime::format_duration(duration));
+        } else {
+            solp::parse_file(path, consumer);
+        }
     }
     Ok(())
 }
 
-fn validate(cmd: &ArgMatches) {
-    let only_problems = cmd.get_flag("problems");
-
-    let mut consumer = Validate::new(only_problems);
-    scan_path(cmd, &mut consumer);
-}
-
-fn info(cmd: &ArgMatches) {
-    let mut consumer = Info::new();
-    scan_path(cmd, &mut consumer);
-}
-
-fn nuget(cmd: &ArgMatches) {
-    let only_mismatched = cmd.get_flag("mismatch");
-    let fail_if_mismatched = cmd.get_flag("fail");
-    let mut consumer = Nuget::new(only_mismatched);
-    scan_path(cmd, &mut consumer);
-    if consumer.mismatches_found && fail_if_mismatched {
-        std::process::exit(exitcode::SOFTWARE);
-    }
-}
-
-fn scan_path<C: Consume + Display>(cmd: &ArgMatches, consumer: &mut C) {
-    if let Some(path) = cmd.get_one::<String>(PATH) {
-        if let Ok(metadata) = fs::metadata(path) {
-            if metadata.is_dir() {
-                let now = Instant::now();
-                let empty = String::default();
-                let extension = cmd.get_one::<String>("ext").unwrap_or(&empty);
-                solp::parse_dir(path, extension, consumer);
-
-                print!("{consumer}");
-
-                let duration = now.elapsed().as_millis();
-                let duration = Duration::from_millis(duration as u64);
-                println!("{:>2} {}", "elapsed:", humantime::format_duration(duration));
-            } else {
-                solp::parse_file(path, consumer);
-            }
-        };
-    }
-}
-
-fn print_completions(matches: &ArgMatches) {
+fn print_completions(matches: &ArgMatches) -> Result<()> {
     let mut cmd = build_cli();
     let bin_name = cmd.get_name().to_string();
     if let Some(generator) = matches.get_one::<Shell>("generator") {
         generate(*generator, &mut cmd, bin_name, &mut io::stdout());
     }
+    Ok(())
 }
 
 fn build_cli() -> Command {
+    #![allow(non_upper_case_globals)]
     command!(crate_name!())
         .arg_required_else_help(true)
         .version(crate_version!())
